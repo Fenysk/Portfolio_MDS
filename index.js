84f8e809b9a1e97ca22dd27f6924d6438f5e6ee3
Fenysk <minefenysk@gmail.com>
2022-12-31 09:42:12 +0100

    Divers modifications

--- index.js
+++ index.js
@@ -15,3 +15,33 @@ button_navigation.addEventListener('click', () => {
 
     }
 });
+
+
+var cards = document.querySelectorAll('.card');
+
+var codeColor = 'linear-gradient(to bottom, rgba(253, 72, 0, 0.2), rgba(255, 156, 0, 0.2))';
+var videoColor = 'linear-gradient(to bottom, rgba(3, 118, 187, 0.2), rgba(30, 229, 206, 0.2))';
+var designColor = 'linear-gradient(to bottom, rgba(87, 0, 129, 0.2), rgba(202, 42, 122, 0.2))';
+
+
+
+cards.forEach(function(card) {
+    card.addEventListener('mouseover', function() {
+        var pageHome = document.querySelector('.page--home');
+        
+        if (card.classList.contains('card--code')) {
+            pageHome.style.background = codeColor;
+          } else if (card.classList.contains('card--video')) {
+            pageHome.style.background = videoColor;
+          } else if (card.classList.contains('card--design')) {
+            pageHome.style.background = designColor;
+          }
+
+    });
+
+    card.addEventListener('mouseout', function() {
+        var pageHome = document.querySelector('.page--home');
+        pageHome.style.background = 'linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 1))';
+    });
+});
+
